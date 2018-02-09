22c71ebddbad0385728efad63a184e49bf414762
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-09 16:56:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,8 +7,8 @@ def prime?(int)
     return true
   elsif
     range = (1...int).to_a
-    range.each do |x|
-      return false if int % x == 0
+    range.all? do |x|
+      return true if int % x != 0
     end
     
  
