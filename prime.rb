13f72a24243a4ee5c073d4ae891797a8e7c3c17c
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-09 16:21:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 # Add  code here!
 def prime?(int)
-  range = (1...int).to_a
-  range.none? do |x|
+  if int < 0
+    return false
+  else
+    range = (1...int).to_a
+    range.none? do |x|
     int % x == 0
   end
   
