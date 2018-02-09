0bbd2b37ab29635b5b085a4fac48672cf6196155
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-09 16:59:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,8 +6,8 @@ def prime?(int)
     return false
   elsif int == 2 || int == 3
     return true
-  elsif range.all? { |x|
-      int % x != 0}
+  elsif range.all? {|x| int % x != 0}
+    return true
     end
     
  
