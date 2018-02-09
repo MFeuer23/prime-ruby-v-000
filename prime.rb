45baf2d6b02aea013d95465082c668b69e9387a6
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-09 16:23:43 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Add  code here!
 def prime?(int)
-  if int =< 0 || int == 1 || int == 2
+  if int <= 0 || int == 1 || int == 2
     return false
   else
     range = (1...int).to_a
     range.none? do |x|
-    int % x == 0
+      int % x == 0
+    end
   end
-end
   
   
 end
