c527f4a5ec343bb226dcff65a2833f2962874efa
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-09 17:55:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,9 @@
 # Add  code here!
 def prime?(int)
 #  range = (1...int).to_a
-# if int <= 1
-#    return false
+if int <= 1
+    return false
+  end
 #  elsif int == 2 || int == 3
 #    return true
 #  elsif range.each {|i| return false if int % i == 0}
