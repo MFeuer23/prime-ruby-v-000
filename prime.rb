73fffac14851c4f383fc98e0553a39f6fc6b5e9f
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-09 18:01:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,24 +1,14 @@
 
 # Add  code here!
 def prime?(int)
-#  range = (1...int).to_a
-if int <= 1
+  if int <= 1
     return false
   end
-#  elsif int == 2 || int == 3
-#    return true
-#  elsif range.each {|i| return false if int % i == 0}
-#    true
-    
-  
-    
-#  end
-
-(2..int).each do |i| 
-if int % i == 0 && i < int
-    return false
+  (2..int).each do |i| 
+    if int % i == 0 && i < int
+      return false
+    end
   end
-end
   true
   
 
